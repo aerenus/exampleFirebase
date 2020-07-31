c8364dc2500d4f66642795d67a1c8d4f93758ff4
Eren FAIKOGLU <e@erenus.net>
2020-07-31 14:13:51 +0300

    Example Project 2

---
+++ exampleFirebase2/exampleFirebase2/exampleFirebase2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  exampleFirebase2
+//
+//  Created by Eren FAIKOGLU on 31.07.2020.
+//  Copyright © 2020 Eren FAIKOGLU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
